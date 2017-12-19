8428d572647e609c81cc6adc26ec9b5b49a0e0d6
charles <charles.bernard@cri-paris.org>
2017-12-19 14:05:22 +0100

    fetch p-val tables

---
+++ data-raw/fetch_MIC_pval_tables.R
@@ -0,0 +1,44 @@
+
+# Fetch p-value tables from official MINE website
+# Turn them into internal data
+
+# This script recquires an internet connection to work
+# ------------------------------------------------------------------------------------
+
+library(RCurl);
+library(stringr);
+library(readr);
+library(devtools);
+
+# Get all lines of the source code of the page containing the links to the pval-tables
+# ------------------------------------------------------------------------------------
+website <- "http://www.exploredata.net"
+internet_directory <- file.path(website, "Downloads/P-Value-Tables");
+source_code <- readLines(internet_directory);
+
+# ------------------------------------------------------------------------------------
+# Get each link to a pval-table
+table_match <- str_extract(source_code, '/ftp/Pvalues/.*[.]csv');
+table_relative_link <- table_match[-which(is.na(table_match))];
+downloadable_link <- paste(website, table_relative_link, sep = "");
+n_table <- length(downloadable_link);
+
+# ------------------------------------------------------------------------------------
+# The Key of each table is the sample size
+sample_size <- str_extract(downloadable_link, '[0-9]{2,4}');
+
+# ------------------------------------------------------------------------------------
+# Read and store each pval-table
+pval_tables <- list();
+for(i in 1:n_table) {
+  curr_file <- read.csv(downloadable_link[i], sep = ",");
+  end <- length(curr_file$X) - 1;
+  pval_tables[[i]] <- data.table(MIC = curr_file$X[11:end],
+                           pval = curr_file$X.1[11:end],
+                           conf = curr_file$X.2[11:end]);
+}
+names(pval_tables) <- sample_size;
+
+# ------------------------------------------------------------------------------------
+# Save object in mage/R/sysdata.rda
+use_data(pval_tables, internal = TRUE);
