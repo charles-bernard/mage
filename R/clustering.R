3c1b35937581df832008899bfdef16cba6963483
charles <charles.bernard@cri-paris.org>
2018-01-03 16:04:37 +0100

    init

--- R/clustering.R
+++ R/clustering.R
@@ -1,3 +1,96 @@
 # mage R package
 
+#' @title phenograph_clustering
+#'
+#' @description uses the phenograph algorithm to perform an automated clustering
+#' of the table of scores
+#'
+#' @param x data.frame; the table of scores
+#'
+#' @param ngb number of nearest neighbours
+#'
+#' @return
+#' a class vector corresponding to the partition of the table of scores
+#'
+#' @importFrom Rphenograph Rphenograph
+#' @importFrom igraph membership
+phenograph_clustering <- function(x, ngb = 50) {
+  x <- x[, 3:ncol(x)];
+  Rphenograph_out <- Rphenograph(x[, 3:ncol(x)], k = ngb);
+  partition <- membership(Rphenograph_out[[2]]);
+  names(partition) <- NULL
+  return(partition);
+}
+
+#' @title get_relevant_k
+#'
+#' @description call NbClust package to return a list of relevant k
+#' according to different methodologies
+#'
+#' @param x data.frame, the table of scores
+#' @param kmax maximun number of clusters allowed
+#'
+#' @param ix boolean vector of indexes to which x must be reduced (sampling output)
+#' @return
+#' a list of 2 elements
+#' \describe{
+#'   \item{NbClust Output}{A vector of integer containing the best k returned by each method}
+#'   \item{Majority Rule k}{k most often returned}
+#' }
+#'
+#' @importFrom NbClust NbClust
+get_relevant_k <- function(x, ix = NULL, kmax = 12) {
+  x <- x[, 3:ncol(x)];
+  if(!is.null(ix)) {
+    x <- x[ix, ];
+  }
+
+  cat("  Computing relevant nb k of clusters ...\n");
+  relevant_k <- list();
+
+  # Call NbClust
+  # ----------------------------------------------------------------------
+  pdf(NULL) # avoid NbClust graphical output
+  nbclust_out <- NbClust(x,
+                         min.nc = 2, max.nc = kmax,
+                         distance = "euclidean", method = "kmeans",
+                         index = "all");
+  junk <- dev.off();
+
+  # Get the k returned by each indice
+  # ----------------------------------------------------------------------
+  good_ix <- which(nbclust_out$Best.nc[1, ] >= 1 &
+                     nbclust_out$Best.nc[1, ] <= kmax);
+  best_k <- nbclust_out$Best.nc[1, good_ix];
+  best_k_names <- names(best_k);
+  relevant_k$`NbClust Output` <- as.integer(best_k);
+  names(relevant_k$`NbClust Output` ) <- best_k_names;
+
+  # Majority Rule k
+  # ----------------------------------------------------------------------
+  t <- table(relevant_k$`NbClust Output`);
+  relevant_k$`Majority Rule k` <-  as.integer(names(t)[max(which(t == t[which.max(t)]))]);
+  return(relevant_k);
+}
+
+#' @title clara_clustring
+#'
+#' @description use clara algorithm to partition the table of scores into k clusters
+#' around medoids (a more robust version of K-means)
+#'
+#' @param x data.frame; the table of scores
+#'
+#' @param k desired number of clusters
+#'
+#' @return
+#' a class vector corresponding to the partition of the table of scores
+#'
+#' @importFrom fpc claraCBI
+clara_clustering <- function(x, k) {
+  # Find the best partition for k clusters using clara algorithm
+  # ----------------------------------------------------------------------
+  x <- x[, 3:ncol(x)];
+  res.clara <- claraCBI(x, k, usepam = TRUE);
+  return(res.clara$partition);
+}
 
